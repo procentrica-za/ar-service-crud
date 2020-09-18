79ae77e21db60bcca1c9ac9d67e3baad91f0f3a4
Chane <Chane@procentrica.co.za>
2020-09-18 08:52:22 +0200

    Implemented function to post funcloc to database. Altered PostAsset function to send json body to stored procedure in postgres.

--- src/routes.go
+++ src/routes.go
@@ -2,5 +2,6 @@ package main
 
 func (s *Server) routes() {
 	// Routes
+	s.router.HandleFunc("/toAssetRegister", s.handlePostToAssetRegister()).Methods("POST")
 
 }
--- src/types.go
+++ src/types.go
@@ -24,3 +24,45 @@ type dbConfig struct {
 type Config struct {
 	ListenServePort string
 }
+
+type toAssetRegister struct {
+	ID                 string `json:"id"`
+	FunclocID          string `json:"funclocid"`
+	Name               string `json:"name"`
+	Description        string `json:"description"`
+	SerialNo           string `json:"serialno"`
+	Size               string `json:"size"`
+	SizeUnit           string `json:"sizeunit"`
+	Type               string `json:"type"`
+	Class              string `json:"class"`
+	Dimension1Val      string `json:"dimension1val"`
+	Dimension2Val      string `json:"dimension2val"`
+	Dimension3Val      string `json:"dimension3val"`
+	Dimension4Val      string `json:"dimension4val"`
+	Dimension5Val      string `json:"dimension5val"`
+	Dimension6Val      string `json:"dimension6val"`
+	Extent             string `json:"extent"`
+	ExtentConfidence   string `json:"extentconfidence"`
+	TakeOnDate         string `json:"takeondate"`
+	ManufactureDate    string `json:"manufacturedate"`
+	DerecognitionDate  string `json:"derecognitiondate"`
+	DerecognitionValue string `json:"derecognitionvalue"`
+}
+
+type toAssetRegsiterList struct {
+	Alist []toAssetRegister `json:"assets"`
+}
+type Funcloc struct {
+	FunclocID   string `json:"funclocid"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+}
+
+type FunclocList struct {
+	Flist []Funcloc `json:"funcloc"`
+}
+
+type toAssetRegisterResult struct {
+	Success bool   `json:"success"`
+	Message string `json:"message"`
+}
